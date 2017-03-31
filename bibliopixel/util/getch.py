b1852842d2caa1c944022b7c5b9423d3b9926fb8
Tom Ritchford <tom@swirly.com>
2017-03-31 15:19:22 +0200

    Add getch utility to read keyboard input.

---
+++ bibliopixel/util/getch.py
@@ -0,0 +1,22 @@
+from ..threads.threads import Loop
+
+# http://stackoverflow.com/a/32671356/43839
+
+try:
+    from msvcrt import getch  # try to import Windows version
+
+except ImportError:
+    def getch():   # define non-Windows version
+        import sys, tty, termios
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
+
+def keyboard_loop(callback, **kwds):
+    return Loop(lambda: callback(getch()), **kwds)
